6d07188baf4d74181234d7ce3c2baf8e7a531669
KimberlyW618 <kimw618@gmail.com>
2023-04-14 00:11:05 -0700

    commitmappinglines

--- sample_map/static/js/logic.js
+++ sample_map/static/js/logic.js
@@ -4,7 +4,6 @@ console.log("working");
 // Create the map object with a center and zoom level.
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
-
 /*
 // another way to make this > This method is useful when needing to add multiple tile layers, or a background images to map(s)
 // Create the map object with a center and zoom level.
